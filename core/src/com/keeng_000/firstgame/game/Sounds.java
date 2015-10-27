5652412649fc4c4957a6e2867713631fbe81ce12
selcuk69 <yakisikli-selcuk69@hotmail.de>
2015-10-27 18:07:16 +0100

    selcuk

---
+++ core/src/com/keeng_000/firstgame/game/Sounds.java
@@ -0,0 +1,12 @@
+package com.keeng_000.firstgame.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
+/**
+ * Created by Selcuk on 27.10.2015.
+ */
+public class Sounds {
+    Sound wavSound = Gdx.audio.newSound(Gdx.files.internal("data/turk.mp3"));
+
+}
