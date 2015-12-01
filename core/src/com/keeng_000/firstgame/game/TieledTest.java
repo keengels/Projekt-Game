b37833eabc74c52fcbaf450e2b22edbb6aa7a943
follnoob <follnoob@gmail.com>
2015-12-01 08:28:17 +0100

    Kleinere Map
    Habe die Map verkleinert. Jetzt sollte sie überall in der Linken unteren Ecke sein.

--- core/src/com/keeng_000/firstgame/game/TieledTest.java
+++ core/src/com/keeng_000/firstgame/game/TieledTest.java
@@ -30,17 +30,18 @@ public class TieledTest extends ApplicationAdapter implements InputProcessor {
 
         tiledMap = new TmxMapLoader().load("test.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
         camera = new OrthographicCamera();
         camera.setToOrtho(false);
-        camera.translate(-624f, 0);
-        camera.zoom = 0.2f;
+        //camera.zoom = 0.5f;
         camera.update();
+        tiledMapRenderer.setView(camera);
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render() {
-        camera.translate(0.3f, 0f);
+        //camera.translate(0.3f, 0f);
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
